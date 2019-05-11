b1f5d17fa7bee53e4315a57d6ded5ffad22b2225
田沼　和樹 <tanuma@tanumakazukinoMacBook-Air.local>
2019-05-11 23:41:42 +0900

    ジェネリクスを使わないで全ての型のインスタンスを格納できるクラスを作った場合

--- Main.java
+++ Main.java
@@ -1,16 +1,8 @@
-import java.util.*;
-
 public class Main{
     public static void main(String[] args){
-	Hero h1 = new Hero("ミナト");
-	Sword s = new Sword("はがねの剣");
-	h1.setSword(s);
-	System.out.println("装備:" + h1.getSword().getName());
-	System.out.println("clone()で複製します");
-	Hero h2 = h1.clone();
-	System.out.println("コピー元の勇者の剣の名前を変えます");
-	h1.getSword().setName("ひのきのぼう");
-	System.out.println("コピー先とコピー元の勇者の剣の名前を表示します");
-	System.out.println("コピー元" + h1.getSword().getName() + "/コピー先:" + h2.getSword().getName() );
+	Pocket p = new Pocket();
+	p.put("1192");
+	String s = (String ) p.get();
+	System.out.println(s);
     }
 }
---
+++ Pocket.java
@@ -0,0 +1,5 @@
+public class Pocket{
+    private Object data;
+    public void put(Object d){ this.data = d; }
+    public Object get(){ return this.data; }
+}
