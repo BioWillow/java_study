b44dd626b6cc3cbd6d5aa6e14249bf1da0043487
田沼　和樹 <tanuma@tanumakazukinoMacBook-Air.local>
2019-05-11 19:27:10 +0900

    練習3-3

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Main{
     public static void main(String[] args){
 	//次の情報を格納する適切なコレクションをList,Set,Mapから選択
@@ -11,5 +12,28 @@ public class Main{
 	  (2)Set
 	  (3)Map
 	 */
+
+	//斎藤,鈴木をHeroクラスからインスタンス化してArrayListに格納して順番に取り出す
+	/*
+	Hero h1 = new Hero("斎藤");
+	Hero h2 = new Hero("鈴木");
+	List<Hero> heros = new ArrayList<Hero>();
+	heros.add(h1);
+	heros.add(h2);
+	for(Hero h : heros){
+	    System.out.println(h.getName());
+	}
+	*/
+
+	//斎藤、鈴木をインスタンス化して倒した敵の数とペアでコレクションに格納して取り出す
+	Hero h1 = new Hero("斎藤");
+	Hero h2 = new Hero("鈴木");
+	Map<Hero, Integer> heros = new HashMap<Hero, Integer>();
+	heros.put(h1,3);
+	heros.put(h2,7);
+	for(Hero key : heros.keySet()){
+	    int value = heros.get(key);
+	    System.out.println(key.getName() + "が倒した敵=" + value);
+	}
     }
 }
