80a2d87d06aa739c18ff4145977b843aeb8fc30f
田沼　和樹 <tanuma@tanumakazukinoMacBook-Air.local>
2019-05-11 22:28:44 +0900

    toStringメソッドのオーバーライド

--- Hero.java
+++ Hero.java
@@ -1,5 +1,19 @@
 public class Hero{
     private String name;
-    public Hero(String name){ this.name = name;}
+    private int hp;
+
+    Hero(String name, int hp){
+	this.name = name;
+	this.hp = hp;
+    }
+    Hero(String name){
+	this(name, 100);
+    }
+
     public String getName(){ return this.name; }
+    //toStringのオーバーライド
+    public String toString(){
+	return "勇者" + this.name + "/HP" + this.hp;
+    }
+
 }
