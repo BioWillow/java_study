205d676d1095de5176bf4f0d20ce0cac12e84e50
田沼　和樹 <tanuma@tanumakazukinoMacBook-Air.local>
2019-05-12 15:55:40 +0900

    練習6-2

--- Main.java
+++ Main.java
@@ -9,8 +9,14 @@ interface Func2{
 public class Main{
     public static void main(String[] args){
 	FuncList funclist = new FuncList();
-	Func1 f1 = FuncList::isOdd;
-	Func2 f2 = funclist::addNamePrefix;
+	Func1 f1 = (int x) -> {return (x % 2 == 1);};
+	Func2 f2 = (boolean male, String name) -> {
+	    if(male = true){
+		return "Mr." + name;
+	    }else {
+		return "Ms." + name;
+	    }
+	};
 	System.out.println(f1.call(19));
 	System.out.println(f2.call(true,"Williams"));
     }
