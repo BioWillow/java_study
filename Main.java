ae450f9978535eb6c69d2553862c19eea38c125b
田沼　和樹 <tanuma@tanumakazukinoMacBook-Air.local>
2019-05-12 15:33:00 +0900

    ラムダ式の省略記法とブロック外変数へのアクセス

--- Main.java
+++ Main.java
@@ -4,6 +4,11 @@ public class Main{
 	//「2つの引数の差を求める処理」の実態を生み出し代入する
 	MyFunction func = (int a, int b) -> {return a - b;};
 	int a = func.call(5,3);
-	System.out.println(a);
+	System.out.println("5-3="+a);
+
+	double pi = 3.1415;
+	MyFunction2 func2 = x -> x * x * pi;
+	double b = func2.call(3);
+	System.out.println("半径3の円の面積は"+b);
     }
 }
--- MyFunction2.java
+++ MyFunction2.java
@@ -1,7 +1,7 @@
 //MyFunction型を使えるように
 //SAMインタフェースを定義する
 
-public interface MyFunction{
+public interface MyFunction2{
     //格納したい関数と同じ引数と戻り値の抽象メソッドを準備
-    public abstract int call(int x,int y);
+    public abstract double call(int x);
 }
