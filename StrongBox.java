cfdd3fe5952dd2531697b38d4ed828c449914e3f
田沼　和樹 <tanuma@tanumakazukinoMacBook-Air.local>
2019-05-12 14:54:56 +0900

    練習5-2

---
+++ StrongBox.java
@@ -0,0 +1,32 @@
+public class StrongBox<E>{
+    private E item;
+    private KeyType keyType;
+    private long count;
+    public E get(){
+	this.count++;
+	switch(this.keyType){
+	case PADLOCK:
+	    if(count < 1024)return null;
+		break;
+	case BUTTON:
+	    if(count < 10000)return null;
+		break;
+	case DIAL:
+	    if(count < 30000)return null;
+		break;
+	case FINGER:
+	    if(count < 1000000)return null;
+		break;
+	}
+	this.count = 0;
+
+	return this.item;
+    }
+    public void put(E item){
+	this.item = item;
+    }
+
+    StrongBox(KeyType key){
+	this.keyType = key;
+    }
+}
