1e40b16d49d2e0cadfcd7b974225cfcd7ffcfd4c
田沼　和樹 <tanuma@tanumakazukinoMacBook-Air.local>
2019-05-12 15:16:49 +0900

    関数格納用のインタフェースの利用

--- Main.java
+++ Main.java
@@ -5,9 +5,10 @@ public class Main{
     }
     public static void main(String[] args){
 	//subメソッドの処理ロジックを変数funcに代入する
-	IntBinaryOperator func = Main::sub;
+	MyFunction func = Main::sub;
 	//変数funcに格納されている処理ロジックを引数5と3で実行する
-	int a = func.applyAsInt(5, 3);
+	//呼び出しはMyFunctionで定義したメソッドで
+	int a = func.call(5, 3);
 	System.out.println(a);
     }
 }
---
+++ MyFunction.java
@@ -0,0 +1,7 @@
+//MyFunction型を使えるように
+//SAMインタフェースを定義する
+
+public interface MyFunction{
+    //格納したい関数と同じ引数と戻り値の抽象メソッドを準備
+    public abstract int call(int x, int y);
+}
