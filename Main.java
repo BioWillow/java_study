d6a5babae9c2a10be6f7e73314cc13a7e9606698
田沼　和樹 <tanuma@tanumakazukinoMacBook-Air.local>
2019-05-12 16:32:46 +0900

    リフレクションの練習

--- Main.java
+++ Main.java
@@ -1,15 +1,19 @@
+import java.lang.reflect.*;
 public class Main{
-    public static void main(String[] args){
-	Class<?> info1 = String.class;
-	System.out.println(info1.getSimpleName());
-	System.out.println(info1.getName());
-	System.out.println(info1.getPackage().getName());
-	System.out.println(info1.isArray());
-
-	Class<?> info2 = info1.getSuperclass();
-	System.out.println(info2.getName());
-
-	Class<?> info3 = args.getClass();
-	System.out.println(info3.isArray());
+    public static void main(String[] args)throws Exception {
+	Class<?> clazz = RefSample.class;
+	//引数一つのコンストラクタを受け取り、インスタンスを取得する
+	Constructor<?> cons = clazz.getConstructor(int.class);
+	RefSample rs = (RefSample) cons.newInstance(256);
+	//timesフィールドに関するFieldを受けとり読み書き
+	Field f = clazz.getField("times");
+	f.set(rs,2);
+	System.out.println(f.get(rs));
+	//引数二つのhelloメソッドを受け取り呼び出す
+	Method m = clazz.getMethod("hello",String.class, int.class);
+	m.invoke(rs,"reflection!",128);
+	//クラスやメソッドへの修飾を調べる
+	boolean pubc = Modifier.isPublic(clazz.getModifiers());
+	boolean finm = Modifier.isFinal(m.getModifiers());
     }
 }
