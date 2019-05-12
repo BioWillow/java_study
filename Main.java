5360731711dfd26a036b806f2fd45f23a40e3e4a
田沼　和樹 <tanuma@tanumakazukinoMacBook-Air.local>
2019-05-12 15:20:54 +0900

    ラムダ式

--- Main.java
+++ Main.java
@@ -1,14 +1,9 @@
 import java.util.function.*;
 public class Main{
-    public static int sub(int a,int b){
-	return a - b;
-    }
     public static void main(String[] args){
-	//subメソッドの処理ロジックを変数funcに代入する
-	MyFunction func = Main::sub;
-	//変数funcに格納されている処理ロジックを引数5と3で実行する
-	//呼び出しはMyFunctionで定義したメソッドで
-	int a = func.call(5, 3);
+	//「2つの引数の差を求める処理」の実態を生み出し代入する
+	MyFunction func = (int a, int b) -> {return a - b;};
+	int a = func.call(5,3);
 	System.out.println(a);
     }
 }
